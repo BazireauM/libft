5c09900dd8cce0ce7b346e6f8dd6bd5e95c2fb18
BazireauM <mathiasbazireau.pro@gmail.com>
2022-11-03 22:23:53 +0100

    fini

--- ft_split.c
+++ ft_split.c
@@ -12,91 +12,82 @@
 
 #include "libft.h"
 
-int		ft_word_count(char const *s, char c)
+char	*fill_split(char *split, char const *s, int a, int len)
 {
-	int	i;
-	int	cnt;
+	int		i;
 
 	i = 0;
-	cnt = 0;
+	while (len > 0)
+	{
+		split[i] = s[a - len];
+		i++;
+		len--;
+	}
+	split[i] = '\0';
+	return (split);
+}
+
+int		get_n_split(char const *s, char c)
+{
+	int	i;
+	int	n_split;
+
+	i = 0;
+	n_split = 0;
 	while (s[i])
 	{
 		if (s[i] == c)
 			i++;
 		else
 		{
-			cnt++;
+			n_split++;
 			while (s[i] && s[i] != c)
 				i++;
 		}
 	}
-	return (cnt);
+	return (n_split);
 }
 
-char	*ft_word_make(char *word, char const *s, int k, int word_len)
+char	**ft_split2(char **final, char const *s, char c, int n_split)
 {
 	int		i;
+	int		b;
+	int		len;
 
+	b = 0;
 	i = 0;
-	while (word_len > 0)
-		word[i++] = s[k - word_len--];
-	word[i] = '\0';
-	return (word);
-}
-
-char	**ft_split2(char **result, char const *s, char c, int word_num)
-{
-	int		i;
-	int		k;
-	int		word_len;
-
-	i = 0;
-	k = 0;
-	word_len = 0;
-	while (s[k] && i < word_num)
+	len = 0;
+	while (s[i] && b < n_split)
 	{
-		while (s[k] && s[k] == c)
-			k++;
-		while (s[k] && s[k] != c)
+		while (s[i] && s[i] == c)
+			i++;
+		while (s[i] && s[i] != c)
 		{
-			k++;
-			word_len++;
+			i++;
+			len++;
 		}
-		if (!(result[i] = (char *)malloc(sizeof(char) * (word_len + 1))))
+		final[b] = malloc(sizeof(char) * (len + 1));
+		if(!final[b])
 			return (NULL);
-		ft_word_make(result[i], s, k, word_len);
-		word_len = 0;
-		i++;
+		fill_split(final[b], s, i, len);
+		len = 0;
+		b++;
 	}
-	result[i] = 0;
-	return (result);
+	final[b] = 0;
+	return (final);
 }
 
 char	**ft_split(char const *s, char c)
 {
-	int		word_num;
-	char	**result;
+	int	n_split;
+	char	**final;
 
 	if (s == 0)
 		return (NULL);
-	word_num = ft_word_count(s, c);
-	if (!(result = (char **)malloc(sizeof(char *) * (word_num + 1))))
+	n_split = get_n_split(s, c);
+	final = malloc(sizeof(char *) * (n_split + 1));
+	if (!final)
 		return (NULL);
-	ft_split2(result, s, c, word_num);
-	return (result);
+	ft_split2(final, s, c, n_split);
+	return (final);
 }
-/*
-int main(void)
-{
-	char **test;
-	test = ft_split("---test-test----test-test----test---test-test-",'-');
-	printf("0 : %s\n", test[0]);
-	printf("1 : %s\n", test[1]);
-	printf("2 : %s\n", test[2]);
-	printf("3 : %s\n", test[3]);
-	printf("4 : %s\n", test[4]);
-	printf("5 : %s\n", test[5]);
-	printf("6 : %s\n", test[6]);
-	printf("7 : %s\n", test[7]);
-}
-*/
