210586f5b2ae3c7627bf0b585aafb7607e84491a
Mathias Bazireau <mbazirea@k1r4p11.42mulhouse.fr>
2022-11-04 18:17:11 +0100

    fini

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: mbazirea <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/01 17:48:18 by mbazirea          #+#    #+#             */
-/*   Updated: 2022/11/04 16:48:58 by mbazirea         ###   ########.fr       */
+/*   Updated: 2022/11/04 18:16:42 by mbazirea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,27 @@
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	size_t	b;
 	size_t	a;
+	int		b;
 	char	*final;
 
+	if (start > ft_strlen(s))
+	{
+		final = malloc(sizeof(char));
+		if (!final)
+			return (NULL);
+		final[0] = 0;
+		return (final);
+	}
 	final = malloc(sizeof(char) * (int) len + 1);
 	if (!final)
 		return (NULL);
-	b = 0;
 	a = 0;
-	while (s[b + start] && start < ft_strlen(s))
+	b = start;
+	while (s[b] && a < len)
 	{
-		if (b >= start && a < len)
-		{
-			final[a] = s[b];
-			a++;
-		}
+		final[a] = s[b];
+		a++;
 		b++;
 	}
 	final[a] = '\0';
